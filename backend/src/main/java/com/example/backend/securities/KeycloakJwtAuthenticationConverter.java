6bec3dfeb48bcd98f41e7619016f899ecd860a3a
Thái Viết Phát <m0tnamk09@gmail.com>
2026-03-09 16:17:00 +0700

    update secutiry

---
+++ backend/src/main/java/com/example/backend/securities/KeycloakJwtAuthenticationConverter.java
@@ -0,0 +1,48 @@
+package com.example.backend.securities;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt source) {
+        return new JwtAuthenticationToken(
+                source,
+                Stream.concat(
+                        new JwtGrantedAuthoritiesConverter().convert(source).stream(),
+                        extractResourceRoles(source).stream()
+                ).collect(Collectors.toSet())
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        var resourceAccess = jwt.<Map<String, Object>>getClaim("resource_access");
+
+        if (resourceAccess == null || resourceAccess.get("account") == null) {
+            return Set.of();
+        }
+
+        var eternal = (Map<String, Object>) resourceAccess.get("account");
+        var roles = (Collection<String>) eternal.get("roles");
+
+        if (roles == null) return Set.of();
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.replace("-", "_")))
+                .collect(Collectors.toSet());
+    }
+}
