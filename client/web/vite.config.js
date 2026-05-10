0ac1da7107ee3ba54bafe569b6edede31a1b8af0
Thái Viết Phát <m0tnamk09@gmail.com>
2026-05-10 13:09:40 +0700

    fix vercel

--- client/web/vite.config.js
+++ client/web/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], build: { chunkSizeWarningLimit: 2000, rollupOptions: { output: { manualChunks(id) { if (id.includes('node_modules')) { return id.toString().split('node_modules/')[1].split('/')[0].toString(); } } } } },
   optimizeDeps: {
     include: ['sockjs-client'],
   },
